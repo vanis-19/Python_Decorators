e12ae46be50959d371db45b00af5bf746b24dce6
vanis-19 <70938255+vanis-19@users.noreply.github.com>
2020-11-13 21:04:32 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,3 +1,15 @@
+def smart_dev(func):
+    def inner(a,b):
+        if b==0:
+            print(a can not divide by zero')
+        else:
+             print(a/b)
+        func(a,b)
+    return inner              
+                  
+                  
+
+
 def add(a,b):
     print(a/b)
 
